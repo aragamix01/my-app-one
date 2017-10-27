97b16261fd191cc4c6a4b1025783fffcbb5cba07
aragamix01 <aragamix01@gmail.com>
2017-10-27 15:45:49 +0700

    path

--- Server.js
+++ Server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+app.get('/shopping', function (req, res) {
+  res.send('shopping')
+})
 app.listen(process.env.PORT || 8080);
